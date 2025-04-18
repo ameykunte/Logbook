1bfd1ed9db37811652daa049329b3558e1dca562
Amey Kunte <amey.k@research.iiit.ac.in>
2025-04-18 22:50:08 +0530

    frontend changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,35 @@
+// src/App.jsx
+
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
 import LoginPage from './components/Auth/LoginPage';
-import PrivateRoute from './components/Auth/PrivateRoute';
 import SignUpPage from './components/Auth/SignUpPage';
+import PrivateRoute from './components/Auth/PrivateRoute';
 import Dashboard from './components/Layout/Dashboard';
 import Search from './components/Layout/SearchLogs';
 
 function App() {
   return (
     <Routes>
+      {/* Public landing page */}
+      <Route path="/" element={<LandingPage />} />
+
+      {/* Auth pages */}
       <Route path="/signup" element={<SignUpPage />} />
       <Route path="/login" element={<LoginPage />} />
+
+      {/* Protected dashboard */}
       <Route
-        path="/"
+        path="/dashboard/*"
         element={
           <PrivateRoute>
             <Dashboard />
           </PrivateRoute>
         }
       />
+
+      {/* Protected search view */}
       <Route
         path="/search"
         element={
@@ -27,6 +38,9 @@ function App() {
           </PrivateRoute>
         }
       />
+
+      {/* Redirect any unknown route back to landing */}
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 }
