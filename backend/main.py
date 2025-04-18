853193756728aa7e611d37932216ed1d193fa845
Sudhankd <121023932+Sudhankd@users.noreply.github.com>
2025-04-18 10:57:15 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -4,14 +4,20 @@ from routes.relationship import relationship_router
 from routes.auth import auth_router
 from fastapi import FastAPI, Request, Depends
 import os
+from dotenv import load_dotenv
+
 from routes.search import search_router
 
+
+load_dotenv()
 app = FastAPI()
 
 # Enable CORS
+# Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Adjust this for production
+    allow_origins=[os.environ.get('REACT_FRONTEND_URL'), # Adjust this for production
+                   "http://localhost:3000"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
